b46d6331f807a0decb20d687cfd084c294478169
Secret Dev <131434750+DaDev123@users.noreply.github.com>
2025-10-24 21:14:10 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,9 @@
 #include <exlaunch.hpp>
 
+if (al::isPlayingBgm(stageScene)) {
+            al::stopAllBgm(stageScene, 0);
+}
+
 extern "C" void exl_main(void* x0, void* x1) {
     /* Setup hooking environment. */
     exl::hook::Initialize();

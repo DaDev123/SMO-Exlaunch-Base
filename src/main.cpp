fa48660973694f8596d0d67ad931987c68f7439f
Secret Dev <131434750+DaDev123@users.noreply.github.com>
2025-10-24 21:19:13 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include <exlaunch.hpp>
 
 #include "Library/Bgm/BgmLineFunction.h"
-
-if (al::isPlayingBgm(stageScene)) {
-            al::stopAllBgm(stageScene, 0);
-}
+HOOK_DEFINE_TRAMPOLINE(StageSceneInitHook) {
+        static void Callback(StageScene *stageScene, const al::SceneInitInfo& info) {
+              if (al::isPlayingBgm(stageScene)) {
+                  al::stopAllBgm(stageScene, 0);
+               }
+           Orig(stageScene, info);
+        }
 
 extern "C" void exl_main(void* x0, void* x1) {
     /* Setup hooking environment. */
     exl::hook::Initialize();
+    StageSceneInitHook::InstallAtSymbol("_ZN10StageScene4initERKN2al13SceneInitInfoE");
 }

87c73c47a2b2a7c82b7827695cd604e3ebcc3143
Secret Dev <131434750+DaDev123@users.noreply.github.com>
2025-10-24 21:42:11 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 #include "Project/Scene/SceneInitInfo.h"
 #include "Scene/StageScene.h"
 
-HOOK_DEFINE_TRAMPOLINE(StageSceneInitHook) {
+HOOK_DEFINE_TRAMPOLINE(StageSceneControlHook) {
     static void Callback(StageScene *stageScene, const al::SceneInitInfo& info) {
         if (al::isPlayingBgm(stageScene)) {
             al::stopAllBgm(stageScene, 0);
@@ -16,5 +16,5 @@ HOOK_DEFINE_TRAMPOLINE(StageSceneInitHook) {
 extern "C" void exl_main(void* x0, void* x1) {
     /* Setup hooking environment. */
     exl::hook::Initialize();
-    StageSceneInitHook::InstallAtSymbol("_ZN10StageScene4initERKN2al13SceneInitInfoE");
+    StageSceneControlHook::InstallAtSymbol("_ZN10StageScene7controlEv");
 }

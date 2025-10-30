3a7604d837134f66178f20e046616c0f32e0293d
DaDev123 <dadev123@github.com>
2025-10-30 12:02:04 +0100

    wtf

--- src/server/SocketBase.hpp
+++ src/server/SocketBase.hpp
@@ -41,7 +41,7 @@ class SocketBase {
         const char *sock_ip;
 
         u16 port;
-        u8 socket_log_state = SocketLogState::UNINITIALIZED;
+        u8 socket_log_state = SOCKET_LOG_UNINITIALIZED;
         s32 socket_log_socket;
 
         int sock_flags;

f4b24eeb28a7fe0fcb92cca77721cf550dcffb33
Secret Dev <131434750+DaDev123@users.noreply.github.com>
2025-11-12 18:26:03 +0100

    Refactor movie playback in StageScene control
    Refactor movie playback logic to prevent immediate playback and add a check for object existence.

--- src/main.cpp
+++ src/main.cpp
@@ -5,36 +5,22 @@
 #include "Scene/StageScene.h"
 #include "Library/Scene/SceneObjUtil.h"
 
-// Forward declare MoviePlayer since we don't have the header
 class MoviePlayer {
 public:
     void play(const char* name);
 };
 
-HOOK_DEFINE_TRAMPOLINE(StageSceneInit) {
-    static void Callback(StageScene *stageScene, const al::SceneInitInfo& info) {
-        Orig(stageScene, info);
-        
-        // Don't play immediately - the movie system might not be ready yet
-        // Try moving this to control() with a frame delay
-    }
-};
-
-static int frameDelay = 0;
+static bool hasAttempted = false;
 
 HOOK_DEFINE_TRAMPOLINE(StageSceneControl) {
     static void Callback(StageScene *stageScene) {
         Orig(stageScene);
         
-        if (frameDelay == 60) { // Wait 60 frames (~1 second)
+        if (!hasAttempted) {
+            // Just check if the object exists without trying to play
             al::ISceneObj* obj = al::getSceneObj(stageScene, 0x24);
-            if (obj) {
-                MoviePlayer* player = reinterpret_cast<MoviePlayer*>(obj);
-                player->play("glitch");
-            }
-            frameDelay++; // Prevent replaying
-        } else if (frameDelay < 60) {
-            frameDelay++;
+            // Log or breakpoint here to see if obj is valid
+            hasAttempted = true;
         }
         
         if (al::isPlayingBgm(stageScene)) {
@@ -45,6 +31,5 @@ HOOK_DEFINE_TRAMPOLINE(StageSceneControl) {
 
 extern "C" void exl_main(void* x0, void* x1) {
     exl::hook::Initialize();
-    StageSceneInit::InstallAtSymbol("_ZN10StageScene4initERKN2al13SceneInitInfoE");
     StageSceneControl::InstallAtSymbol("_ZN10StageScene7controlEv");
 }
